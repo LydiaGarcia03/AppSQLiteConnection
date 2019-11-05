d50e6b0aa8c97ae0792463b0c03d8adcb537aa02
LydiaGarcia03 <lydiagarcia.chan@gmail.com>
2019-11-05 09:48:40 -0200

    RawQuery no Debug

--- app/src/main/java/br/edu/ifsc/lydiagarcia/bd_notas/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/lydiagarcia/bd_notas/MainActivity.java
@@ -1,8 +1,12 @@
 package br.edu.ifsc.lydiagarcia.bd_notas;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +25,37 @@ public class MainActivity extends AppCompatActivity {
                 "titulo VARCHAR NOT NULL," +
                 "texto VARCHAR);");
 
-        db.execSQL("INSERT INTO  notas VALUES (null, 'Desespero', '');");
+        //db.execSQL("INSERT INTO  notas VALUES (null, 'Desespero', '');");
+
+        // O INSERT abaixo é o mesmo que o INSERT acima.
+        // Porém, a classe ContentValues evita SQLInjections e é mais organizada
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("titulo", "Desespero++");
+        contentValues.put("texto", "Agora eu estou mais desesperada");
+
+        db.insert("notas", null, contentValues);
+
+        Cursor cursor = db.rawQuery("SELECT * FROM notas", null, null);
+
+        // Já que o cursor teria parado na última linha de inserção
+        cursor.moveToFirst();
+
+        String id, titulo, texto;
+
+        while(!cursor.isAfterLast()){
+
+            id = cursor.getString(cursor.getColumnIndex("id"));
+            titulo = cursor.getString(cursor.getColumnIndex("titulo"));
+            texto = cursor.getString(cursor.getColumnIndex("texto"));
+
+            Log.d("tabelaNotas", id + " " + titulo + " " + texto);
+
+            cursor.moveToNext();
+
+        }
+
+
 
     }
 }
