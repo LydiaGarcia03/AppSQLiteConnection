ad210dc48bbec5c65678b4441da813a0824dafc5
Lydia Garcia <lydiagarcia.chan@gmail.com>
2019-11-05 08:18:52 -0300

    Primeiro commit - não finalizado

---
+++ app/src/main/java/br/edu/ifsc/lydiagarcia/bd_notas/MainActivity.java
@@ -0,0 +1,25 @@
+package br.edu.ifsc.lydiagarcia.bd_notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db.openOrCreateDatabase("mydb", null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "titulo VARCHAR NOT NULL," +
+                "texto VARCHAR);");
+
+    }
+}
